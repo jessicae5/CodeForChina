a4826d05e7ce0747dcd8ee3fbbe0952ba92d5d03
jessicae5 <jessicaezhao@gmail.com>
2019-07-14 09:58:16 +0800

    Create blank.py

---
+++ blank.py
@@ -0,0 +1,22 @@
+#Import Statements
+import turtle
+import random
+
+#Global Variables
+screen = turtle.Screen()
+jess = turtle.Turtle()
+jess.speed(1000)
+screen.colormode(255)
+
+#Helper Functions
+
+#Main Function
+def main():
+     #####Code Goes HERE#####
+
+     ########################
+     screen.exitonclick()
+     return 0
+
+if __name__ == "__main__":
+     main()
