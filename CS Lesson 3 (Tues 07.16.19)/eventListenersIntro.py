a366f19fd13f44d76883e1c153a03ebb47f4b00b
jessicae5 <jessicaezhao@gmail.com>
2019-07-17 13:00:27 +0800

    Lesson 3

--- CS Lesson 3 (Tues 07.16.19)/eventListenersIntro.py
+++ CS Lesson 3 (Tues 07.16.19)/eventListenersIntro.py
@@ -8,23 +8,7 @@ turtle = turtle.Turtle()
 def up():
     turtle.forward(100)
 
-def down():
-    turtle.left(90)
-    turtle.forward(100)
-
-def left():
-    turtle.left(90)
-    turtle.forward(100)
-
-def right():
-    turtle.left(90)
-    turtle.forward(100)
-
 screen.onkey(up, "Up")
-screen.onkey(down, "Down")
-screen.onkey(right, "Right")
-screen.onkey(left, "Left")
-
 screen.listen()
 
 screen.exitonclick()
