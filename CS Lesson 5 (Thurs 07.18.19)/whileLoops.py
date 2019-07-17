6ee5d3e713085225fc85c2dc2d537f32d5f6b652
jessicae5 <jessicaezhao@gmail.com>
2019-07-18 00:40:49 +0800

    Lesson 5

---
+++ CS Lesson 5 (Thurs 07.18.19)/whileLoops.py
@@ -0,0 +1,18 @@
+#Import Statements
+import turtle
+import random
+
+#Global Variables
+
+#Helper Functions
+def printWhile(min,max):
+  now = max
+  #TODO: Finish the while loop
+  
+#Main Function
+def main():
+    printWhile(0,5)
+    printWhile(6,7)
+
+if __name__ == "__main__":
+     main()
